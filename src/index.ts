0c0eac313518425d89ef5be6acf99704deecd154
Victor Bussiki <victorbussiki@gmail.com>
2023-11-10 08:26:12 -0300

    feat(Query Builder): inner query

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,15 @@ const queryBuilder = MongoQueryBuilder.query({parentKey: 'erp'})
 //queryBuilder.or('standard', false)
 //queryBuilder.and('$and', {$and: queryBuilder.internalAnd({'delivery': false}).$and})
 
-const standardQuery = MongoQueryBuilder.query({parentKey: 'erp'}).and('staff', false).and('delivery', false).build()
-const standardQueryKey = Object.keys(standardQuery)[0] as keyof MatchingQuery
+queryBuilder.or('delivery', true)
+queryBuilder.or('staff', true)
+queryBuilder.or('item.canceled', true)
 
-queryBuilder.or(standardQueryKey, standardQuery[standardQueryKey]!)
+queryBuilder.inner('$or')
+    .and('delivery', false)
+    .and('staff', false)
+    .innerBuild()
 
 const query = queryBuilder.build()
 
-console.log(query)
-
-console.log(JSON.stringify(query))
+console.log(JSON.stringify(query, undefined, 4))
--- src/model/MongoQueryBuilder.ts
+++ src/model/MongoQueryBuilder.ts
@@ -1,9 +1,15 @@
-export type MongoQuery = Record<string, string | number | boolean | MongoQuery [] >
-export type MatchingQuery = {$and?: Array<MongoQuery>, $or?: Array<MongoQuery>}
+export type MongoRegex = {[key: string]: {$regex: string, $options: string}}
+
+export type MongoQuery = Record<string, string | number | boolean | MongoQuery[] >
+
+export type MatchingQuery = {$and: Array<MongoQuery>, $or: Array<MongoQuery>}
+
+export type InnerMatchingQuery = Partial<MatchingQuery>
+
 export type QueryOptions = {parentKey?: string}
 
 export class MongoQueryBuilder {
-    private constructor (private props: MatchingQuery , private options?: QueryOptions) {}
+    private constructor (private props: MatchingQuery , private queryOptions?: QueryOptions) {}
 
     static query(options?: QueryOptions) {
         const props = {
@@ -53,9 +59,78 @@ export class MongoQueryBuilder {
         return this
     }
 
+    inner(operator: keyof MatchingQuery): InnerQueryBuilder {
+        return new InnerQueryBuilder(operator, this)
+    }
+
     build(): MatchingQuery {
         return this.props
     }
 
+    get options(): QueryOptions | undefined {
+        return this.queryOptions
+    }
+
+}
+
+class InnerQueryBuilder {
+    private readonly childQuery: InnerMatchingQuery     
+
+    constructor(private readonly operator: keyof MatchingQuery, private readonly motherQuery: MongoQueryBuilder) {
+        this.childQuery = {
+            $and: [{}],
+            $or: [{}] 
+        }
+    }
+
+    and(key: string, value: string | number | boolean| MongoQuery[]): InnerQueryBuilder {
+        const query: MongoQuery = {}
+
+        const queryOperatorRegex = /^\$[a-z]+$/
+
+        const prefix = queryOperatorRegex.test(key) ? '' : this.motherQuery.options?.parentKey?.concat('.') || ''
+
+        query[`${prefix}${key}`] = value;
+
+        const $andIsEmpty = (Object.keys(this.childQuery.$and![0]).length === 0)
+
+        if($andIsEmpty) {
+            this.childQuery.$and![0] = query
+        } else {
+            this.childQuery.$and!.push(query)
+        }
+        return this
+    }
+
+    or(key: string, value: string | number | boolean| MongoQuery[]): InnerQueryBuilder {
+        const query: MongoQuery = {}
+
+        const queryOperatorRegex = /^\$[a-z]+$/
+
+        const prefix = queryOperatorRegex.test(key) ? '' : this.motherQuery.options?.parentKey?.concat('.') || ''
+
+        query[`${prefix}${key}`] = value
+
+        const $orIsEmpty = (Object.keys(this.childQuery.$or![0]).length === 0)
+
+        if($orIsEmpty) {
+            this.childQuery.$or![0] = query
+        } else {
+            this.childQuery.$or!.push(query)
+        }
+
+        return this
+    }
+    innerBuild(): MongoQueryBuilder {
+        
+        const childQueryKey = Object.keys(this.childQuery)[0] as keyof InnerMatchingQuery
+        const childQueryValues = this.childQuery[childQueryKey]
+
+        const embeddedQuery = this.operator === '$and' 
+            ? this.motherQuery.and(childQueryKey, childQueryValues!) 
+            : this.motherQuery.or(childQueryKey, childQueryValues!)        
+
+        return embeddedQuery
+    }
 }
 
